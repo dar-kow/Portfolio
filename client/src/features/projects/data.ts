78be033bd4eb643098c91359795556f391536959
Darek <darecki9k@gmail.com>
2025-03-22 13:55:43 +0100

    Update project links and add new project entry for SDET initiative

--- client/src/features/projects/data.ts
+++ client/src/features/projects/data.ts
@@ -29,8 +29,8 @@ export const projects: Project[] = [
       pl: "Proste narzędzie do analizy finansowej z przetwarzaniem danych w czasie rzeczywistym",
       en: "Simple financial analysis tool with real-time data processing",
     },
-    link: "https://github.com/darek9k/M-A-F",
-    demo: "http://srv10.mikr.us:20267",
+    link: "https://github.com/dar-kow/M-A-F",
+    demo: "http://maf.sdet.pl",
   },
   {
     title: { pl: "Portfolio Website", en: "Portfolio Website" },
@@ -38,8 +38,8 @@ export const projects: Project[] = [
       pl: "Portfolio z efektami w stylu Matrix zbudowane przy użyciu React",
       en: "Personal portfolio with Matrix-style animations built using React",
     },
-    link: "https://github.com/darek9k/portfolio",
-    demo: "https://portfolio.bieda.it",
+    link: "https://github.com/dar-kow/portfolio",
+    demo: "https://portfolio.sdet.pl",
   },
   {
     title: { pl: "Testy Playwright - aplikacji MAF", en: "Playwright Testing - MAF App" },
@@ -51,5 +51,15 @@ export const projects: Project[] = [
     demo: "",
     // link: "https://github.com/playwright-maf",
     // demo: "https://portfolio.bieda.it",
+  },
+  {
+    title: { pl: "SDET - Strefa dla testerów, których korci na więcej", en: "SDET - The Zone for Testers Who Want More" },
+    description: {
+      pl: "Projekt w fazie budowy. Już wkrótce pokaże, jak zbudować prostą, ale skuteczną stronę startową inicjującą projekt. Założeniem jest nie tylko stworzenie atrakcyjnego interfejsu, ale także zbieranie subskrybentów i zaproszenie chętnych do współtworzenia. W tle działa solidny backend z panelem administratora – zarządzanie danymi, autoryzacja oparta o JWT i baza PostgreSQL gwarantują nowoczesne podejście. Start odliczania to pierwszy, ale najważniejszy krok, który motywuje do działania. Stay tuned!",
+      en: "Project Under Construction. Coming soon: a showcase on how to build a simple yet effective landing page to kickstart your project. The aim is not only to create an attractive interface but also to gather subscribers and invite interested collaborators. Behind the scenes, a robust backend featuring an admin panel—with data management, JWT-based authentication, and PostgreSQL—ensures a modern approach. The countdown to launch is the first, and most important, step that motivates action. Stay tuned!",
+    },
+    link:"",
+    // link: "https://github.com/dar-kow/portfolio",
+    demo: "https://sdet.pl",
   },
 ];
