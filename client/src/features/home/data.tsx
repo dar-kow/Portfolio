73ee344de2c6841df0b0f566af8da0ba6c96c85b
Darek <darecki9k@gmail.com>
2025-04-02 18:55:57 +0200

    Refactor homeMessages: Remove colons from project and CV messages, and comment out downloadCV entry

--- client/src/features/home/data.tsx
+++ client/src/features/home/data.tsx
@@ -4,19 +4,19 @@ export const homeMessages = {
         en: "Welcome to my portfolio!",
     },
     projects: {
-        pl: "Poznaj moje projekty:",
-        en: "Check out my projects:",
+        pl: "Poznaj moje projekty",
+        en: "Check out my projects",
     },
     cv: {
-        pl: "Przejrzyj moje CV:",
-        en: "View my resume:",
+        pl: "Przejrzyj moje CV",
+        en: "View my resume",
     },
     contact: {
         pl: "Skontaktuj się ze mną",
         en: "Contact me",
     },
-    downloadCV: {
-        pl: "Pobierz CV",
-        en: "Download CV",
-    },
+    // downloadCV: {
+    //     pl: "Pobierz CV",
+    //     en: "Download CV",
+    // },
 };
